29ee41b1fcdf561ed30c2332550ae3feec8aee74
Benafsh Husain <benafsh@Benafshs-MacBook-Pro.local>
2019-09-11 10:10:18 -0700

    Added multiprocess Netextractor code that calculates MI and GMM

---
+++ NetExtractor_multiprocess.py
@@ -0,0 +1,166 @@
+import os
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+import itertools
+
+import numpy as np
+from scipy import linalg
+'''
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot as plt
+'''
+import pandas as pd
+from random import randint
+from sklearn.metrics.cluster import adjusted_mutual_info_score
+from sklearn.metrics.cluster import normalized_mutual_info_score
+
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from scipy.stats import gaussian_kde
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+from scipy.stats import kde
+from mpl_toolkits.mplot3d import Axes3D
+
+
+from sklearn import mixture
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot as plt
+from sklearn import metrics
+import pandas as pd
+import multiprocessing as mp
+
+
+color_iter = ['navy', 'darkorange']
+
+#Read input GEM
+
+data = pd.read_csv("./kidney-GEM/kidney_GEM.txt", sep='\t')
+data = data.iloc[:, 1:]
+data = data.transpose()
+data = data.fillna(0)
+data = data.fillna(0)
+data[data < 0] = 0
+global_max = data.max().max()
+global_min = data.min().min()
+
+print(data.shape, global_max, global_min)
+
+
+#Read annotation files with metadata
+
+data_annot = pd.read_csv("./kidney-GEM/kidney-labels.txt", sep='\t')
+
+print(data_annot.shape)
+
+labels = data_annot.iloc[:, 1]
+
+c = []
+
+
+#Initialize file to write MI values
+f = open('mi_gmm_kidney.csv', 'w')
+
+
+#Add color labels for specific tissue samples
+for i in labels:
+    if i == 'kich-tumor-tcga':
+        c.append('r')
+    if i == 'kich-normal-tcga':
+        c.append('b')
+    if i == 'kidney-gtex':
+        c.append('b')
+    if i == 'kirc-normal-tcga':
+        c.append('b')
+    if i == 'kirc-tumor-tcga':
+        c.append('r')
+    if i == 'kirp-normal-tcga':
+        c.append('b')
+    if i == 'kirp-tumor-tcga':
+        c.append('r')
+
+count = 0
+
+#Ensuring enough samples have non-zero expression values
+threshold = int(.8 * 927)
+
+index_list = []
+
+#Create list of genes that satisfy threshold criteria
+for i in range(0, 19215):
+    count_i = sum(m > 0 for m in data.ix[:, i])
+    if count_i > threshold:
+        index_list.append(i)
+
+
+def my_func(x):
+    for j in index_list:
+        if j > x:
+            N = np.c_[data.ix[:, x], data.ix[:, j]]
+            #Calculate Mututual information of all samples
+            value = normalized_mutual_info_score(N[:, 0], N[:, 1])
+            #Calculate mixture models. Specifiy the number of clusters: n_components
+            dpgmm = mixture.BayesianGaussianMixture(n_components=2, covariance_type='full', max_iter=100, ).fit(N)
+            Y_ = dpgmm.predict(N)
+            unique, counts = np.unique(Y_, return_counts=True)
+            S = metrics.silhouette_score(N, Y_)
+            x_1 = []
+            y_1 = []
+            x_2 = []
+            y_2 = []
+            #Ensure enough samples fall into each cluster and have a minimum intercluster score
+            if len(counts) == 2 and counts[0] > 150 and counts[1] > 150 and S > 0.5:
+                count_cluster_1 = 0
+                count_cluster_2 = 0
+                covar = dpgmm.covariances_
+                mean = dpgmm.means_
+                v, w = linalg.eigh(covar[0])
+                v = 2. * np.sqrt(2.) * np.sqrt(v)
+                u = w[0] / linalg.norm(w[0])
+                angle = np.arctan(u[1] / u[0])
+                angle = 180. * angle / np.pi  # convert to degrees
+                cos_angle = np.cos(np.radians(180. + angle))
+                sin_angle = np.sin(np.radians(180. + angle))
+                xc = mean[1][0] - mean[0][0]
+                yc = mean[1][1] - mean[0][1]
+                xct = xc * cos_angle - yc * sin_angle
+                yct = xc * sin_angle + yc * cos_angle
+                rad_cc = (xct ** 2 / (v[0] / 2.) ** 2) + (yct ** 2 / (v[1] / 2.) ** 2)
+                #Ensure clusters are mutually separate using their mean and variance
+                if rad_cc > 1:
+                    #Write the edge, MI value, and intercluster score
+                    string_val = '%s,%s,%0.3f,%0.3f\n' % (str(data.columns.values[x]), str(data.columns.values[j]), value, S)
+                    f.write(string_val)
+                    f.flush()
+
+
+#Calling multiprocessing function. Specify number of threads to launch the job.
+def main():
+    pool = mp.Pool(20)
+    result = pool.map(my_func, index_list)
+
+
+
+
+
+if __name__ == "__main__":
+  main()
